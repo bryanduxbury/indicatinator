21489260d9d55a2b2c5f161fb3d05086387ae370
nicole-allard <nicole.allard@gmail.com>
2012-04-24 17:29:14 -0700

    leds and servo both working

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 
 #define FIRST_LED_PIN 14
+#define LAST_LED_PIN 53
 #define NUM_LEDS 40
 #define MIN_DEGREES 5
 #define MAX_DEGREES 167
@@ -23,57 +24,70 @@ void setup() {
 
 // Temporary input parsing for debugging
 void parseInput() {
- int input = Serial.read() - 48;
+ int input = Serial.read();
+ // line feed
+ if (input == 10) {
+   return;
+ }
+ input = input - 48;
  if (first == -1) {
    first = input;
  }
  else {
    percent = (first * 10) + input;
    first = -1;
-   Serial.println(percent);
  }
-}
-
-void loop() {
-  if (Serial.available()) {
-    parseInput();
-    percent = percent/float(100);
-    percent = min(1, max(0, percent));
-    servoDegrees = MAX_DEGREES - (percent * (MAX_DEGREES - MIN_DEGREES));
-
-    numLedsLit = round(percent * float(NUM_LEDS));
-    servo1.write(servoDegrees);
-  }
-  
-  updateLEDs();
 }
 
 void startupSequence() {
   for (int i = 0; i < NUM_LEDS; i++) {
     pinMode(FIRST_LED_PIN + i, OUTPUT);
-//    digitalWrite(FIRST_LED_PIN + i, HIGH);
-//    delay(200);
-//    digitalWrite(FIRST_LED_PIN + i, LOW);
   }
+  
+  calculateExternals(float(0));
+  delay(1000);
+  for (int i = 0; i <= 100; i++) {
+    calculateExternals(float(i));
+    unsigned long endTime = millis() + 15;
+    while (millis() < endTime) {
+      updateLEDs(); 
+    }
+  }
+  for (int i = 180; i >= 0; i--) {
+    calculateExternals(float(i));
+    unsigned long endTime = millis() + 15;
+    while (millis() < endTime) {
+      updateLEDs(); 
+    }
+  }
+}
 
-//  for (int i = 180; i >= 0; i--) {
-//    servoDegrees = i;
-//    numLedsLit = float(servoDegrees)/MAX_DEGREES * NUM_LEDS;
-//    long endTime = millis() + 5;
-//    while (millis() < endTime) {
-//      updateExternals();
-//    }
-//  }
+void loop() {
+  if (Serial.available()) {
+    parseInput();
+    calculateExternals(percent);
+  }
+  
+  updateLEDs();
+}
+
+void calculateExternals(float percent) {
+  percent = percent/float(100);
+  percent = min(1, max(0, percent));
+  servoDegrees = MAX_DEGREES - (percent * (MAX_DEGREES - MIN_DEGREES));
+
+  numLedsLit = round(percent * float(NUM_LEDS));
+  servo1.write(servoDegrees);
 }
   
 void updateLEDs() {
-  for (int i = FIRST_LED_PIN; i < FIRST_LED_PIN+numLedsLit; i+=5) {
-    for (int j = 0; j < 5 && i + j < FIRST_LED_PIN+numLedsLit; j++) {
-      digitalWrite(i + j, HIGH);
+  for (int i = LAST_LED_PIN; i > LAST_LED_PIN-numLedsLit; i-=5) {
+    for(int j = 0; j < 5 && i-j > LAST_LED_PIN-numLedsLit; j++) {
+      digitalWrite(i - j, HIGH);
     }
     delay(1);
-    for (int j = 0; j < 5 && i + j < FIRST_LED_PIN+numLedsLit; j++) {
-      digitalWrite(i + j, LOW);
+    for(int j = 0; j < 5 && i-j > LAST_LED_PIN-numLedsLit; j++) {
+      digitalWrite(i - j, LOW);
     }
   }
 }
